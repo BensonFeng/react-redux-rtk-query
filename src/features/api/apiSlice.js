b6c857655f941f244273170d6e5739d3aa6ef3b9
BensonFeng <bensonfeng@outlook.com>
2022-04-22 20:40:54 -0400

    adding mutation to the slice

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -5,9 +5,36 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3500" }),
   endpoints: (builder) => ({
     getTodos: builder.query({
-      query: () => `/todos`,
+      query: () => "/todos",
+      transformResponse: (res) => res.sort((a, b) => b.id - a.id),
+    }),
+    addTodo: builder.mutation({
+      query: (todo) => ({
+        url: "/todos",
+        method: "POST",
+        body: todo,
+      }),
+    }),
+    updateTodo: builder.mutation({
+      query: (todo) => ({
+        url: `/todos/${todo.id}`,
+        method: "PATCH",
+        body: todo,
+      }),
+    }),
+    deleteTodo: builder.mutation({
+      query: ({ id }) => ({
+        url: `/todos/${id}`,
+        method: "DELETE",
+        body: id,
+      }),
     }),
   }),
 });
 
-export const { useGetTodosQuery } = apiSlice;
+export const {
+  useGetTodosQuery,
+  useAddTodoMutation,
+  useUpdateTodoMutation,
+  useDeleteTodoMutation,
+} = apiSlice;
